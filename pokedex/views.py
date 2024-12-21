a8e720c9a9d29c812f26285dc8e55a147916e922
IVAN DELGADO <iadelgadod@puce.edu.ec>
2024-12-21 09:24:48 -0500

    laboratorio8ivanp2

--- pokedex/views.py
+++ pokedex/views.py
@@ -1,13 +1,18 @@
 from django.http import HttpResponse
 from django.template import loader
-from .models import Pokemon
+from .models import Pokemon, Trainer
 
 
 
 def index(request):
-    pokemons = Pokemon.objects.order_by('type')
+    pokemons = Pokemon.objects.all()
+    trainers= Trainer.objects.all()
     template = loader.get_template('index.html')
-    return HttpResponse(template.render({'pokemons': pokemons}, request))
+    return HttpResponse(template.render({
+        'pokemons': pokemons,
+        'trainers': trainers
+        }, 
+        request))
 
 def pokemon(request, pokemon_id):
     pokemon = Pokemon.objects.get(pk = pokemon_id)
@@ -17,4 +22,10 @@ def pokemon(request, pokemon_id):
     }
     return HttpResponse(template.render(context, request))
 
-
+def trainer_detail(request, trainer_id):
+    trainer = Trainer.objects.get(pk = trainer_id)
+    template = loader.get_template('display_trainer.html')
+    context = {
+        'trainer': trainer
+    }
+    return HttpResponse(template.render(context, request))
